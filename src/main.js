3347a40b8acf605bb3e6074e1882b1978dfcc8d6
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-17 20:40:25 -0600

    separate promise logic
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,38 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import GiphyService from './GiphyService.js'; 
+
+
+function clearFields() {
+  $('#keyword').val("");
+  $('.showErrors').text("");
+  $('.showGifs').text("");
+}
 
 $(function() {
   $('#searchButton').on('click', function() {
     const searchQuery = $('#keyword').val();
-    $('#keyword').val("");
-    $('.showGifs').html("");
-
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchQuery}&limit=5&offset=0&rating=g&lang=en`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
+    clearFields();
+    let promise = GiphyService.getGif(searchQuery);
     promise.then(function(response) {
       const body = JSON.parse(response);
       body.data.forEach(function(gif) {
-        $('.showGifs').append(`<img src="${gif.images.original.url}" alt="${gif.title}"><br><br>`)
-      });
+        $('.showGifs').append(`<img src="${gif.images.original.url}" alt="${gif.title}"><br><br>`);
+      }); 
     }, function(error) {
-      $('.showerrors').text('There was an error processing your request: ${error}');
-      
-    }
-    
-  }
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
   });
 });
 
